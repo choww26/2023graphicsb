e49a072d96812d5eeab6cb8716f27f8b81b31768
choww26 <jamielin2003@gmail.com>
2023-05-11 16:35:32 +0800

    week13

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -1,35 +1,45 @@
 #include <stdio.h>
 #include <GL/glut.h>
+#include "glm.h" ///week13 step02-2
+GLMmodel * head =NULL;///week13 step02-2
+GLMmodel * body =NULL;///week13 step02-2
+GLMmodel * left_arm =NULL;
+
 float teapotX = 0, teapotY = 0;
 FILE * fout = NULL;
 FILE * fin = NULL;
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-    glPushMatrix();
-        glTranslatef(teapotX, teapotY, 0);
-        glutSolidTeapot(0.3);
-    glPopMatrix();
+        if(head==NULL){///week13 step02-2 將模型讀進來
+            head = glmReadOBJ("model/head.obj");
+            body = glmReadOBJ("model/body.obj");
+            left_arm = glmReadOBJ("model/left_arm.obj");
+            ///glmUnitize(head);
+            ///glmUnitize(body);
+        }
+        glPushMatrix();
+            glScalef(0.1,0.1,0.1);
+            glmDraw(head, GLM_MATERIAL);///week13 step02-2
+            glmDraw(body, GLM_MATERIAL);
+            glmDraw(left_arm, GLM_MATERIAL);
+        glPopMatrix();
     glutSwapBuffers();
 }
 void mouse(int button, int state, int x, int y)
 {
-    teapotX = (x-150)/150.0;
-    teapotY = (150-y)/150.0;
-    if(state==GLUT_DOWN){///if mouse press down
-        if(fout==NULL) fout = fopen("file4.txt", "w");
-        fprintf(fout, "%f %f\n", teapotX, teapotY);
+    if(state==GLLUT_DOWN){
+        oldX = x;
+        oldY = y;
     }
-    display();
 }
-void keyboard(unsigned char key, int x, int y)///step02-2
+void motion(int x, int y)///step02-2
 {
-    if(fin==NULL){
-        fclose(fout);
-        fin = fopen("file4.txt", "r");
-    }
-    fscanf(fin, "%f %f\n", &teapotX, &teapotY);
-    display();
+    teapotX += (x - oldX)/150.0;
+    teapotX -= (y - oldY)/150.0;
+    oldX = x;
+    oldY = y;
+    glutPostRedisplay();
 }
 int main(int argc, char** argv)
 {
@@ -38,7 +48,7 @@ int main(int argc, char** argv)
     glutCreateWindow("week12");
 
     glutDisplayFunc(display);
-    glutKeyboardFunc(keyboard);
+    glutMotionFunc(motion);
     glutMouseFunc(mouse);
     glutMainLoop();
 }
